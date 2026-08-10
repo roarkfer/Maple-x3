c88ef71cb1a456d82d8e66dd6b1ffee52a1e79bc
roarkfer <roarkfer12358@gmail.com>
2026-08-09 21:50:20 -0600

    Delete setup-freeink.sh

--- setup-freeink.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-rm -rf freeink-sdk
-git clone https://github.com/Free-Ink/freeink-sdk.git freeink-sdk
-cd freeink-sdk
-git checkout e62f6c16f0ed477ffbe1ad15fa838f32433adfc3
